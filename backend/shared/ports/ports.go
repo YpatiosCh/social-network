bac581d7a825f3766d8958b018fb012e84000069
Vagelis Stefanopoulos <vstefanopoulos@yahoo.gr>
2025-11-25 16:14:04 +0200

    readme k8s and load balancer

--- backend/shared/ports/ports.go
+++ backend/shared/ports/ports.go
@@ -3,17 +3,11 @@ This is a namespace to hold port addreses for all services.
 */
 package ports
 
-// Container ports used with individual docker containers
+// Client ports
 const (
 	Users         string = "users:50051"
-	Forum         string = "forum:50052"
-	Chat          string = "chat:"
-	Notifications string = "notifications:"
-	Example       string = "example:1234"
-)
-
-// Kube service DNS
-const (
-	UsersDNS string = "users.social-network.svc.cluster.local:50051"
-	UsersDb  string = "users-db.social-network.svc.cluster.local:5432"
+	Forum         string = "forum:50051"
+	Chat          string = "chat:50051"
+	Notifications string = "notifications:50051"
+	Example       string = "example:50051"
 )
