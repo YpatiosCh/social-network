509f3553769046e0a86e76cdc0b05e1192686b38
kvamvasa <cathringo@gmail.com>
2026-01-07 17:46:43 +0200

    added forgotten variables in get group posts

--- backend/services/posts/internal/application/posts.go
+++ backend/services/posts/internal/application/posts.go
@@ -26,8 +26,9 @@ func (s *Application) CreatePost(ctx context.Context, req models.CreatePostReq) 
 	groupId.Int64 = req.GroupId.Int64()
 	if req.GroupId == 0 {
 		groupId.Valid = false
+	} else {
+		groupId.Valid = true
 	}
-	groupId.Valid = true
 
 	audience := ds.IntendedAudience(req.Audience.String())
 
