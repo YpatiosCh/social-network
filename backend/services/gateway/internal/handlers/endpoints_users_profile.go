ce879075b624c6a7dc65d46f8c60c5e7620b19f4
kvamvasa <cathringo@gmail.com>
2026-01-14 19:05:06 +0200

    update user profile accepts avatar id

--- backend/services/gateway/internal/handlers/endpoints_users_profile.go
+++ backend/services/gateway/internal/handlers/endpoints_users_profile.go
@@ -201,6 +201,7 @@ func (s *Handlers) updateUserProfile() http.HandlerFunc {
 			LastName    ct.Name        `json:"last_name"`
 			DateOfBirth ct.DateOfBirth `json:"date_of_birth"`
 			About       ct.About       `json:"about" validate:"nullable"`
+			AvatarId    ct.Id          `json:"avatar_id"`
 
 			AvatarName string `json:"avatar_name"`
 			AvatarSize int64  `json:"avatar_size"`
@@ -221,7 +222,7 @@ func (s *Handlers) updateUserProfile() http.HandlerFunc {
 			return
 		}
 
-		var AvatarId ct.Id
+		AvatarId := httpReq.AvatarId
 		var uploadURL string
 		if httpReq.AvatarSize != 0 {
 			exp := time.Duration(10 * time.Minute).Seconds()
